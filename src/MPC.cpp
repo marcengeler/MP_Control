20c408e4bafed87eea8f195317f93d08880fe724
Marc Engeler <marc.engeler@gmail.com>
2017-11-07 23:54:53 +0100

    debugging options

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -226,6 +226,7 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
   // Cost
   auto cost = solution.obj_value;
   std::cout << "Cost " << cost << std::endl;
+  std::cout << solution.x << std::endl;
 
   // TODO: Return the first actuator values. The variables can be accessed with
   // `solution.x[i]`.
