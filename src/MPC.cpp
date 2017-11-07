1045e7d71ee8b0caf91ad30f240b6563f9566cdf
Marc Engeler <marc.engeler@gmail.com>
2017-11-08 00:14:21 +0100

    tuning

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -41,19 +41,19 @@ class FG_eval {
     fg[0] = 0.0;
 	for (unsigned int i = 0; i < N; i++) {
 		// Minimize deviation and change rate
-		fg[0] += 120.0 * CppAD::pow(vars[cte_start + i], 2);
-		fg[0] += 150.0 * CppAD::pow(vars[epsi_start + i], 2);
-		fg[0] += 20.0 * CppAD::pow(vars[v_start + i] - 100.0, 2);
+		fg[0] += 1000.0 * CppAD::pow(vars[cte_start + i], 2);
+		fg[0] += 1000.0 * CppAD::pow(vars[epsi_start + i], 2);
+		fg[0] += 1.0 * CppAD::pow(vars[v_start + i] - 100.0, 2);
 		
 		if (i < N -1) {
 			// Minimize actuators
-			fg[0] += 500.0 * CppAD::pow(vars[delta_start + i], 2);
-			fg[0] += 500.0 * CppAD::pow(vars[a_start + i], 2);
+			fg[0] += 50.0 * CppAD::pow(vars[delta_start + i], 2);
+			fg[0] += 50.0 * CppAD::pow(vars[a_start + i], 2);
 		}
 		
 		if  (i < N - 2) {
-			fg[0] += 10000.0*CppAD::pow(vars[delta_start + i + 1] - vars[delta_start + i], 2);
-			fg[0] += 250.0* CppAD::pow(vars[a_start + i + 1] - vars[a_start + i], 2);
+			fg[0] += 250000.0*CppAD::pow(vars[delta_start + i + 1] - vars[delta_start + i], 2);
+			fg[0] += 5000.0* CppAD::pow(vars[a_start + i + 1] - vars[a_start + i], 2);
 		}
 	}
 	
