15025aa4fab851493e1ad13ea55d632b8518b8a2
Marc Engeler <marc.engeler@gmail.com>
2017-11-08 00:05:15 +0100

    copied code form lesson to rule out typos

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -42,7 +42,7 @@ class FG_eval {
 	for (unsigned int i = 0; i < N; i++) {
 		// Minimize deviation and change rate
 		fg[0] += 3000.0 * CppAD::pow(vars[cte_start + i], 2);
-		fg[0] += 1500.0 * CppAD::pow(vars[epsi_start + i], 2);
+		fg[0] += 3000.0 * CppAD::pow(vars[epsi_start + i], 2);
 		fg[0] += CppAD::pow(vars[v_start + i] - 100.0, 2);
 		
 		if (i < N -1) {
@@ -65,37 +65,49 @@ class FG_eval {
     fg[1 + cte_start] = vars[cte_start];
     fg[1 + epsi_start] = vars[epsi_start];
 	
-	for (unsigned int i = 0; i < N - 1; i++) {
-		// Using AD type in order tu use later on algorithms
-		// provided by the library
-		AD<double> x1 = vars[x_start + i + 1];
-		AD<double> y1 = vars[y_start + i + 1];
-		AD<double> psi1 = vars[psi_start + i + 1];
-		AD<double> cte1 = vars[cte_start + i + 1];
-		AD<double> epsi1 = vars[epsi_start + i + 1];
-		
-		AD<double> x0 = vars[x_start + i];
-		AD<double> y0 = vars[y_start + i];
-		AD<double> psi0 = vars[psi_start + i];
-		AD<double> v1 = vars[v_start + i];
-		AD<double> v0 = vars[v_start + i];
-		AD<double> cte0 = vars[cte_start + i];
-		AD<double> epsi0 = vars[epsi_start + i];
-		
-		AD<double> a0 = vars[a_start + i];
-		AD<double> delta0 = vars[delta_start + i];
+	for (int t = 1; t < N; t++) {
+	  // The state at time t+1 .
+	  AD<double> x1 = vars[x_start + t];
+	  AD<double> y1 = vars[y_start + t];
+	  AD<double> psi1 = vars[psi_start + t];
+	  AD<double> v1 = vars[v_start + t];
+	  AD<double> cte1 = vars[cte_start + t];
+	  AD<double> epsi1 = vars[epsi_start + t];
 
-		AD<double> f0 = coeffs[0] + coeffs[1] * x0 + coeffs[2] * CppAD::pow(x0, 2) + coeffs[3] * CppAD::pow(x0, 3);
-		AD<double> psides0 = CppAD::atan(coeffs[1] + 2 * coeffs[2] * x0 + 3 * coeffs[3] * CppAD::pow(x0, 2));
+	  // The state at time t.
+	  AD<double> x0 = vars[x_start + t - 1];
+	  AD<double> y0 = vars[y_start + t - 1];
+	  AD<double> psi0 = vars[psi_start + t - 1];
+	  AD<double> v0 = vars[v_start + t - 1];
+	  AD<double> cte0 = vars[cte_start + t - 1];
+	  AD<double> epsi0 = vars[epsi_start + t - 1];
 
-		fg[2 + x_start + i] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
-		fg[2 + y_start + i] = y1 - (y0 + v0 * CppAD::sin(psi0) * dt);
-		fg[2 + psi_start + i] = psi1 - (psi0 - 1/Lf * v0 * delta0 * dt);
-		fg[2 + v_start + i] = v1 - (v0 + a0 * dt);
-		fg[2 + cte_start + i] = cte1 - ((f0 - y0) + (v0 * CppAD::sin(epsi0) * dt));
-		fg[2 + epsi_start + i] = epsi1 - ((psi0 - psides0) - 1/Lf * v0 * delta0 * dt);
-    }
-  }
+	  // Only consider the actuation at time t.
+	  AD<double> delta0 = vars[delta_start + t - 1];
+	  AD<double> a0 = vars[a_start + t - 1];
+
+	  AD<double> f0 = coeffs[0] + coeffs[1] * x0;
+	  AD<double> psides0 = CppAD::atan(coeffs[1]);
+
+	  // Here's `x` to get you started.
+	  // The idea here is to constraint this value to be 0.
+	  //
+	  // Recall the equations for the model:
+	  // x_[t] = x[t-1] + v[t-1] * cos(psi[t-1]) * dt
+	  // y_[t] = y[t-1] + v[t-1] * sin(psi[t-1]) * dt
+	  // psi_[t] = psi[t-1] + v[t-1] / Lf * delta[t-1] * dt
+	  // v_[t] = v[t-1] + a[t-1] * dt
+	  // cte[t] = f(x[t-1]) - y[t-1] + v[t-1] * sin(epsi[t-1]) * dt
+	  // epsi[t] = psi[t] - psides[t-1] + v[t-1] * delta[t-1] / Lf * dt
+	  fg[1 + x_start + t] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
+	  fg[1 + y_start + t] = y1 - (y0 + v0 * CppAD::sin(psi0) * dt);
+	  fg[1 + psi_start + t] = psi1 - (psi0 + v0 * delta0 / Lf * dt);
+	  fg[1 + v_start + t] = v1 - (v0 + a0 * dt);
+	  fg[1 + cte_start + t] =
+		  cte1 - ((f0 - y0) + (v0 * CppAD::sin(epsi0) * dt));
+	  fg[1 + epsi_start + t] =
+		  epsi1 - ((psi0 - psides0) + v0 * delta0 / Lf * dt);
+	}
 };
 
 //
