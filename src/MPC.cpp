4421d4a09eb3282bb2076fb28b3639fc584535e3
Marc Engeler <marc.engeler@gmail.com>
2017-11-10 00:30:53 +0100

    tuning*

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -91,8 +91,8 @@ class FG_eval {
 	  AD<double> epsi0 = vars[epsi_start + t - 1];
 
 	  // Only consider the actuation at time t.
-	  AD<double> delta0 = vars[delta_start + t - 1];
-	  AD<double> a0 = vars[a_start + t - 1];
+	  AD<double> delta0 = vars[delta_start + t - 2];
+	  AD<double> a0 = vars[a_start + t - 2];
 
 	  AD<double> f0 = coeffs[0] + coeffs[1] * x0 + coeffs[2] * CppAD::pow(x0, 2) + coeffs[3] * CppAD::pow(x0, 3);
 	  AD<double> psides0 = CppAD::atan(coeffs[1] + 2 * coeffs[2] * x0 + 3 * coeffs[3] * CppAD::pow(x0, 2));
