e66bec1dc90e1d9c33b9fca3ee3d58123bad708d
Marc Engeler <marc.engeler@gmail.com>
2017-11-09 23:50:53 +0100

    tuning

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -56,7 +56,7 @@ class FG_eval {
 		}
 		
 		if  (i < N - 2) {
-			fg[0] += 100000.0*CppAD::pow(vars[delta_start + i + 1] - vars[delta_start + i], 2);
+			fg[0] += 150000.0*CppAD::pow(vars[delta_start + i + 1] - vars[delta_start + i], 2);
 			fg[0] += 1.0* CppAD::pow(vars[a_start + i + 1] - vars[a_start + i], 2);
 		}
 	}
