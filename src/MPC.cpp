f5111a1bfb04ce3516a7162ac46ee1d537d6c694
Marc Engeler <marc.engeler@gmail.com>
2017-11-10 00:37:33 +0100

    tuning

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -182,14 +182,14 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
     constraints_upperbound[i] = 0;
   }
 
-  /*
+  
   x = x + v*cos(psi)*dt;
   y = y + v*sin(psi)*dt;
-  psi = psi - v*delta/Lf *dt;
+  psi = psi + v*delta/Lf *dt;
   v = v + a*dt;
   cte = cte + (v * sin(epsi) * dt);
-  epsi = epsi - v * delta / Lf * dt;
-  */
+  epsi = epsi + v * delta / Lf * dt;
+  
   vars[x_start] = x;
   vars[y_start] = y;
   vars[psi_start] = psi;
